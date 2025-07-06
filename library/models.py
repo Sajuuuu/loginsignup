23cbc6712fadf2487bc2848a070a49f51dacc02d
Sajuuuu <sajupoudel50@gmail.com>
2025-07-06 22:49:50 +0545

    implement dynamic book display and media setup

--- library/models.py
+++ library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    picture = models.ImageField()
+    author = models.CharField(max_length=50,default='Guest')
+    email = models.EmailField(blank=True)
+    description = models.TextField(default='Available in LICT Library')
+
+    def __str__(self):
+        return self.name
+
+
+
--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from. import views
-
+from logSin.settings import DEBUG,STATIC_URL,STATIC_ROOT,MEDIA_URL,MEDIA_ROOT
+from django.conf.urls.static import static
 
 
 urlpatterns =[
     path('library',views.librarypage,name='library'),
 
 ]
+
+if DEBUG:
+    urlpatterns += static(STATIC_URL,document_root = STATIC_ROOT)
+    urlpatterns += static(MEDIA_URL,document_root = MEDIA_ROOT)
