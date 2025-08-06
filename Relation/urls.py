d12092c88a5fcd062d336ffe08863d4f43da4391
Sajuuuu <sajupoudel50@gmail.com>
2025-08-06 22:31:59 +0545

    billing with khalti

--- Relation/urls.py
+++ Relation/urls.py
@@ -9,6 +9,5 @@ path('booklist', views.booklist,name='booklist'),
 path('addauthor',views.addauthor,name='addauthor'),
 path('addbook',views.addbook,name='addbook'),
 path('book/<int:pk>/',views.book_details,name='book_details'),
-
-
+path('verify-khalti-payment/',views.verify_khalti_payment,name='verify_khalti_payment'),
 ]
--- Relation/views.py
+++ Relation/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .forms import PublisherForm,AuthorForm,BookForm
 from .models import Book
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import requests
+import json
+
+#replace with your khalti secret key 
+KHALTI_SECRET_KEY ='Your_secret_key'
+KHALTI_VERIFY_URL ='https://khalti.com/api/v2/epayment/'
+
 
 
 # Create your views here.
@@ -42,3 +51,30 @@ def addbook(request):
 def book_details(request,pk):
      book = get_object_or_404(Book,pk=pk)
      return render(request,'relation/bookdetails.html',{'book' : book})
+
+               
+@csrf_exempt
+def verify_khalti_payment(request):
+    if request.method == 'POST':
+        try:
+            payload=json.loads(request.body)
+            response = requests.posat(
+                KHALTI_VERIFY_URL,
+                JSON={
+                    'token':payload.get("token"),
+                    'amount':payload.get("amount")
+                },
+                headers={
+                    'Authorization' :f'Bearer{KHALTI_SECRET_KEY}',
+                    'Content-Type':'application/json'
+
+                }
+            )
+            verification_data = response.json()
+            if response.status_code == 200 and verification_data.get('status')=='success':
+                return JsonResponse({"success":True})
+            else:
+                return JsonResponse({"success":False},status=400)
+        except Exception as e:
+            return JsonResponse({"success":False,"error":str(e)},status=400)
+    return JsonResponse({"success":False},status=400)
